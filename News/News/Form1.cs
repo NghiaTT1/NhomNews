c6377317c68e4271972a845af500ec93ae44162e
quangduy96 <quangduy96.sr19@gmail.com>
2021-10-30 23:43:48 +0700

    From1.cs
    quang update

--- News/News/Form1.cs
+++ News/News/Form1.cs
@@ -16,6 +16,57 @@ namespace News
             InitializeComponent();
             this.button1.Click += new System.EventHandler(this.button1_Click);
         }
+        private void load()
+        {
+            string url = textBox1.Text;
+            XmlReader xmlreader = XmlReader.Create(url);
+            int index = 0;
+            while (xmlreader.Read())
+            {
+                if (xmlreader.Name == "item")
+                {
+                    XmlReader pReader = xmlreader.ReadSubtree();
+                    String title = "";
+                    String link = "";
+                    while (pReader.Read())
+                    {
+
+                        if (pReader.Name == "title")
+                        {
+                            XmlReader cReader = pReader.ReadSubtree();
+                            while (cReader.Read())
+                            {
+                                if (cReader.Value != "")
+                                {
+                                    listBox1.Items.Add(cReader.Value);
+                                    title = cReader.Value;
+                                }
+
+                            }
+                        }
+
+                        if (pReader.Name == "link")
+                        {
+                            XmlReader cReader = pReader.ReadSubtree();
+                            while (cReader.Read())
+                            {
+                                if (cReader.Value != "")
+                                {
+                                    link = cReader.Value;
+                                }
+
+                            }
+                        }
+
+
+
+                    }
+                    items.Add(new Item { Id = index, Title = title, Link = link });
+                    index++;
+                }
+
+            }
+        }
     }
 }
 
