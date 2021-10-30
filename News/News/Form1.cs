6c11183098c1e1fc2096db842f29b2537f43f02e
NghiaTT1 <93071871+NghiaTT1@users.noreply.github.com>
2021-10-30 23:38:38 +0700

    form1.cs
    Nghia update

--- News/News/Form1.cs
+++ News/News/Form1.cs
@@ -1,20 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
+using System.ServiceModel.Syndication;
 using System.Windows.Forms;
+using System.Xml;
+
 
 namespace News
 {
     public partial class Form1 : Form
     {
+        List<Item> items = new List<Item>();
         public Form1()
         {
             InitializeComponent();
+            this.button1.Click += new System.EventHandler(this.button1_Click);
         }
     }
+}
+
+public class Item
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Link { get; set; }
 }
